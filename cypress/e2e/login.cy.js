a339d6295bf68a23aa4d4ef760d8300b2c85745e
Jose Arthur <arthurjoseb2017@gmail.com>
2026-04-27 09:07:09 -0300

    test: adiciona suite de testes E2E para o fluxo completo do Módulo de Login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,41 @@
+describe('Pagina de login', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/#')
+    })
+    it('Ao digitar seu usuário e senha corretamente, o usuário irá logar na plataforma.', () => {
+        cy.get('[data-testid="login-username"]').type('admin');
+        cy.get('[data-testid="login-password"]').type('admin123');
+        cy.get('[data-testid="login-button"]').click()
+        cy.url().should('include', 'http://localhost:3000/dashboard');
+    })
+
+    it('Ao tentar acessar com um usuário não cadastrado, o sistema deve exibir a mensagem: "Usuário \'usuario\' não encontrado no sistema".', () => {
+        cy.get('[data-testid="login-username"]').type('peter');
+        cy.get('[data-testid="login-password"]').type('homemaranha');
+        cy.get('[data-testid="login-button"]').click();
+        cy.contains('Usuário \'peter\' não encontrado no sistema').should('be.visible');
+    })
+
+    it('Ao digitar um usuário válido, mas com a senha errada, o sistema deve exibir a mensagem: "Senha incorreta!".', () => {
+        cy.get('[data-testid="login-username"]').type('admin');
+        cy.get('[data-testid="login-password"]').type('admin');
+        cy.get('[data-testid="login-button"]').click();
+        cy.contains('Senha incorreta!').should('be.visible');
+    })
+
+    it('Ao tentar efetuar o login sem preencher os dados, o sistema deve solicitar: "Por favor, preencha todos os campos".', () => {
+        cy.get('[data-testid="login-button"]').click();
+        cy.contains('Por favor, preencha todos os campos').should('be.visible');
+    })
+
+    it('O campo de senha deve ocultar visualmente os caracteres digitados.', () => {
+        cy.get('[data-testid="login-password"]').type('admin');
+        cy.get('[data-testid="login-password"]').should('have.attr', 'type', 'password');
+    })
+
+    it('O sistema deve ser acessível e responsivo.', () => {
+        cy.viewport('iphone-x');
+        cy.get('[data-testid="login-username"]').should('be.visible');
+        cy.get('[data-testid="login-button"]').should('be.visible');
+    })
+})
